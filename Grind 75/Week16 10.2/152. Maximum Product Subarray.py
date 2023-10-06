48dc8be4129668f6f75e11ccd0e1a9acf25786e5
eddie <czt1717@gmail.com>
2023-10-06 04:03:27 -0400

    10.6 mac

---
+++ Grind 75/Week16 10.2/152. Maximum Product Subarray.py
@@ -0,0 +1,20 @@
+from typing import Optional, List
+from collections import deque
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        keens, steakhouse = 1, 1
+        maxProduct = nums[0]
+        for n in nums:
+            keensT = max(keens*n, steakhouse*n, n)
+            steakhouse = min(keens * n, steakhouse*n, n)
+            keens = keensT
+            maxProduct = max(maxProduct, keens)
+        return maxProduct
+
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    toPrint = sol.func()
+    print(toPrint)
